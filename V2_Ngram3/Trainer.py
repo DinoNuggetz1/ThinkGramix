e6000c91fdd2e3995301507002dbbfd13879d926
DinoNuggetz1 <krishm2210@gmail.com>
2026-08-18 15:37:13 -0400

    Create Trainer.py

---
+++ V2_Ngram3/Trainer.py
@@ -0,0 +1,15 @@
+def training():
+        with open("dataset1.txt", 'r') as d:
+            for line in d:
+                data = line.strip(string.punctuation).lower().split()
+                for i in range(len(data)-2):
+                    cword = data[i].strip(string.punctuation)
+                    word = data[i + 1].strip(string.punctuation)
+                    word2 = data[i + 2].strip(string.punctuation)
+                    if cword.isdigit() or word.isdigit() or word2.isdigit():
+                        continue 
+                    if cword.isalpha() and word.isalpha() and word2.isalpha():
+                        pair = cword + "-" + word + '-' + word2
+                        res = open("train.txt", 'a')
+                        res.write(pair + "\n")
+training()
